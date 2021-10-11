97bf48eab96b54dc529b4a61abd73be7c53c5e99
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-10-11 12:51:43 -0500

    Agregar archivos de proyecto.

---
+++ Persona.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Personas
+{
+    public class Persona
+    {
+
+        #region Atributos
+        private string nombre;
+        private string telefono;
+        private byte edad;
+        #endregion
+
+        #region Propiedades
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public byte Edad { get => edad;
+            set
+            {
+                if ( value < 18  || value > 100     )
+                {
+                    edad = 18;
+                }
+                else
+                {
+                    edad = value;
+                }                
+            }
+         }
+        #endregion
+
+
+        /// <summary>
+        /// Construtor de la clase persona para inicializar
+        ///  los datos del usuario
+        /// </summary>
+        /// <param name="nombre">Nombre completo de de la persona</param>
+        /// <param name="telefono">Teléfono de la persona</param>
+        /// <param name="edad">Edad de la persona</param>
+        public Persona(string nombre, string telefono, byte edad)
+        {
+            Nombre = nombre;
+            Telefono = telefono;
+            Edad = edad;
+        }
+
+        public override string ToString()
+        {
+            string datos= String.Format( "Nombre: {0} \n Teléfono: {1} \n Edad: {2}",Nombre,Telefono,Edad);
+            return datos ;
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Personas;
+namespace ArreglosObjetos_G3_2022_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Persona[] integranteEquipo1 = new Persona[3];
+
+            integranteEquipo1[0] = new Persona("Armando","552517248",24);
+            integranteEquipo1[1] = new Persona("Pablo", "251015789", 20);
+            integranteEquipo1[2] = new Persona("Carlos", "12345847", 22);
+
+            for(int i=0; i < integranteEquipo1.Length; i++)
+            {
+                Console.WriteLine(integranteEquipo1[i].Nombre);
+            }
+            
+            foreach( Persona integrante in integranteEquipo1    )
+            {
+                Console.WriteLine("Teléfono: {0}", integrante.Telefono );
+                Console.WriteLine("Edad:  {0}", integrante.Edad);
+            }
+
+            Persona[,] integranteG3Equipo = new Persona[2, 2];
+
+            //Integrantes para el equipo1 del Grupo 3
+            integranteG3Equipo[0, 0] = new Persona("Juan", "2510111012", 20);
+            integranteG3Equipo[0, 1] = new Persona("Carlos", "2511111012", 22);
+            //Integrantes para el equipo2 del Grupo 3
+            integranteG3Equipo[1, 0] = new Persona(" Luis  ", "12548789", 20);
+            integranteG3Equipo[1, 1] = new Persona("Hugo", "215245885", 20);
+
+            for( int i=0; i < 2; i++   )
+            {
+                Console.WriteLine("****Equipo {0}*****", i + 1);
+                for(int j=0; j< 2; j++)
+                {
+                    Console.WriteLine("Integrante {0}", j+1);
+                    Console.WriteLine(integranteG3Equipo[i,j]);
+                   // Console.WriteLine("Nombre: {0}, ", integranteG3Equipo[i,j].Nombre);
+                   // Console.WriteLine("Telefono: {0}", integranteG3Equipo[i, j].Telefono);
+                  //  Console.WriteLine("Edad: {0}", integranteG3Equipo[i, j].Edad);
+                }
+            }
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
